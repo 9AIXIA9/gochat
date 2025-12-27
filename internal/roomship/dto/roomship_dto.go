1ed2031469a0041b06a197eabdb29fc713318810
90609 <906094554@qq.com>
2025-12-27 23:51:44 +0800

    docs: Add example values to Swagger documentation for room members

--- internal/roomship/dto/roomship_dto.go
+++ internal/roomship/dto/roomship_dto.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Roomship struct {
-	ID        domain.RoomshipID   `json:"id"`
-	RoomID    kernel.RoomID       `json:"room_id"`
-	UserID    kernel.UserID       `json:"user_id"`
-	Role      domain.RoomshipRole `json:"role"`
-	CreatedAt time.Time           `json:"created_at"`
+	ID        domain.RoomshipID   `json:"id" example:"019b593b-462e-74d6-bfda-0e103a172191"`
+	RoomID    kernel.RoomID       `json:"room_id" example:"019b593b-462e-74d6-bfda-0e103a172192"`
+	UserID    kernel.UserID       `json:"user_id" example:"019b593b-462e-74d6-bfda-0e103a172193"`
+	Role      domain.RoomshipRole `json:"role" example:"member"`
+	CreatedAt time.Time           `json:"created_at" example:"2025-12-26 05:38:19.740"`
 }
 
 func ToRoomshipDTO(roomship *domain.Roomship) *Roomship {
--- internal/roomship/port/http/list_room_members_handler.go
+++ internal/roomship/port/http/list_room_members_handler.go
@@ -11,7 +11,7 @@ import (
 )
 
 type ListRoomMembersRequest struct {
-	RoomID kernel.RoomID `uri:"room_id" validate:"required"`
+	RoomID kernel.RoomID `uri:"room_id" validate:"required" example:"019b593b-462e-74d6-bfda-0e103a172191"`
 }
 
 func (r *ListRoomMembersRequest) Bind(ginContext *gin.Context) error {
@@ -29,7 +29,7 @@ type ListRoomMembersResponseData struct {
 // @Summary      获取房间成员列表
 // @Description  根据房间ID获取房间成员列表
 // @Tags         Roomship
-// @Param        room_id  path        kernel.RoomID                           true  "房间ID"
+// @Param        room_id  path      string     true  "房间ID"    example("019b593b-462e-74d6-bfda-0e103a172190")
 // @Success      200      {object}  api.Response{data=ListRoomMembersResponseData}  "成功返回房间成员列表"
 // @Router       /rooms/{room_id}/members [get]
 func NewListRoomMembersHandler(useCase application.ListRoomMembersUseCase, validator ginutils.Validator) gin.HandlerFunc {
