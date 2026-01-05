6c017a2e9909fe78b5736de3acdb7106d4e0152f
90609 <906094554@qq.com>
2026-01-05 20:16:30 +0800

    feat: Enhance logging by adding trace and user IDs to request completion logs

--- internal/delivery/http/middleware/logger_middleware.go
+++ internal/delivery/http/middleware/logger_middleware.go
@@ -1,10 +1,12 @@
 package middleware
 
 import (
+	"context"
 	"gochat/pkg/ctxutil"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
 
@@ -17,15 +19,48 @@ func NewLoggerMiddleware() gin.HandlerFunc {
 		if path == "" {
 			path = c.Request.URL.Path
 		}
-		zap.L().Info(
-			"request completed",
+
+		fields := []zap.Field{
 			zap.String("method", c.Request.Method),
 			zap.String("path", path),
 			zap.String("query", c.Request.URL.RawQuery),
 			zap.Int("status", c.Writer.Status()),
 			zap.Duration("latency", dur),
-			zap.String("request_id", ctxutil.RequestIDFrom(c.Request.Context()).String()),
 			zap.Error(c.Errors.Last()),
+		}
+
+		if requestID := ctxutil.RequestIDFrom(c.Request.Context()).String(); requestID != "" {
+			fields = append(fields, zap.String("request_id", requestID))
+		}
+
+		if traceID, spanID := getSpanIDAndTraceID(c.Request.Context()); traceID != "" && spanID != "" {
+			fields = append(fields,
+				zap.String("trace_id", traceID),
+				zap.String("span_id", spanID),
+			)
+		}
+
+		if userID := ctxutil.UserIDFrom(c.Request.Context()); userID != "" {
+			fields = append(fields, zap.String("user_id", userID.String()))
+		}
+
+		zap.L().Info(
+			"request completed",
+			fields...,
 		)
 	}
+}
+
+func getSpanIDAndTraceID(ctx context.Context) (string, string) {
+	span := trace.SpanFromContext(ctx)
+	if !span.IsRecording() {
+		return "", ""
+	}
+
+	spanCtx := span.SpanContext()
+	if !spanCtx.IsValid() {
+		return "", ""
+	}
+
+	return spanCtx.TraceID().String(), spanCtx.SpanID().String()
 }
